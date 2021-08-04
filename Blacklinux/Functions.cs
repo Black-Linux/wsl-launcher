58fa45ac01975beb93e8cbb985b6051390da7776
Julian <81520713+Juliaaan2502@users.noreply.github.com>
2021-08-04 13:08:47 +0200

    Add files via upload

---
+++ Blacklinux/Functions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Functions(string[] args)
+        {
+        }
+    }
+}
---
+++ Blacklinux/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using KeyEvents;
+
+namespace Blacklinux
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Title = "Blacklinux";
+            
+            string WslStart = "-d Blacklinux";
+            Process.Start("wsl.exe", WslStart);
+        }
+    }
+}
